da027812c9dbf1c5721921b6bfdf31e4a342fc10
Gustavo Caqui <gustavocaqui94@gmail.com>
2025-10-06 14:52:31 -0500

    add environments

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # cinema_app_03
 
 A new Flutter project.
+
+# Development
+
+## Environment variables
+
+Copy the file `.env.template` to `.env` and add the environment variables.
+
+## Run the app
+
+Run the app with `flutter run`.
+
+## Build the app
+
+Build the app with `flutter build apk`.
+
+## Deploy the app
+
+Deploy the app to the Google Play Store.
---
+++ lib/domain/repositories/movie_repository.dart
@@ -0,0 +1,9 @@
+import 'package:cinema_app_03/domain/entities/movie.dart';
+
+abstract class MovieRepository {
+  // Future<List<Movie>> getNowPlayingMovies();
+  Future<List<Movie>> getNowPlaying({int page = 1});
+  // Future<List<Movie>> getTopRatedMovies();
+  // Future<Movie> getMovieById(int movieId);
+  // Future<List<Movie>> searchMovies(String query);
+}
